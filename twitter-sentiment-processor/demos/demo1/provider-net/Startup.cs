a9006bde521b54b998412877825107960314e345
Saar Shen <xiaomi7732@hotmail.com>
2021-11-10 15:36:52 -0800

    Filter out invalid chars

--- twitter-sentiment-processor/demos/demo1/provider-net/Startup.cs
+++ twitter-sentiment-processor/demos/demo1/provider-net/Startup.cs
@@ -8,9 +8,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Dapr.Client;
-using System.IO;
 using System.Diagnostics;
-using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace Provider
 {
@@ -64,38 +63,26 @@ namespace Provider
 
             async Task Tweet(HttpContext context)
             {
+                Regex validCharacterSet = new Regex(@"^[a-z0-9_-]+$", RegexOptions.IgnoreCase);
                 var client = context.RequestServices.GetRequiredService<DaprClient>();
                 var requestBodyStream = context.Request.Body;
 
-                TwitterTweet tweet = null;
-                string tweetContent = string.Empty;
-                int random = new Random().Next();
+                TwitterTweet tweet = await JsonSerializer.DeserializeAsync<TwitterTweet>(requestBodyStream).ConfigureAwait(false);
 
-                if (random % 2 == 0)
+                string fullText = tweet.FullText;
+                if (string.IsNullOrEmpty(fullText))
                 {
-                    RunSort(5000);
-                    tweet = await JsonSerializer.DeserializeAsync<TwitterTweet>(requestBodyStream);
+                    fullText = tweet.Text;
                 }
-                else
+                Console.WriteLine("Tweet received: {0}: {1}", tweet.ID, fullText);
+
+                if (!validCharacterSet.IsMatch(fullText))
                 {
-                    using (MemoryStream memoryStream = new MemoryStream())
-                    {
-                        await requestBodyStream.CopyToAsync(memoryStream).ConfigureAwait(false);
-                        for (int i = 0; i < 10000; i++)
-                        {
-                            memoryStream.Seek(0, SeekOrigin.Begin);
-                            tweet = await JsonSerializer.DeserializeAsync<TwitterTweet>(memoryStream).ConfigureAwait(false);
-                            tweetContent += tweet.Text;
-                            string ordered = tweet.Text.ToArray().OrderBy(c => c).ToString();
-                        }
-                    }
+                    Console.WriteLine("There is invalid characters other than letter, numbers, underscore(_), and dash(-) signs.");
+                    return;
                 }
-
-                Console.WriteLine("Tweet received: {0}: {1}", tweet.ID, tweet.Text);
-
                 await client.SaveStateAsync<TwitterTweet>(stateStore, tweet.ID, tweet);
                 Console.WriteLine("Tweet saved: {0}: {1}", tweet.ID, tweet);
-
                 return;
             }
         }
